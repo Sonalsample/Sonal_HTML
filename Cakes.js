78051d9d909b06b8b6010f875968679258b2f6cc
Sonal Bhujbal <122430661+Sonalsample@users.noreply.github.com>
2023-01-18 19:22:25 +0530

    Add files via upload

---
+++ Cakes.js
@@ -0,0 +1,17 @@
+function abc()
+{
+alert("Welcome to Ns Cake Shop");
+}
+document.getElementById("myBtn").onclick=abc;
+
+function pqr()
+{
+alert("Your Response is Submited Successfully..!");
+}
+document.getElementById("myBtn").onclick=pqr;
+
+function xyz()
+{
+alert("Signup Done Successfully..!");
+}
+document.getElementById("myBtn").onclick=pqr;
